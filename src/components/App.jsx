8d8aaec3c3fe9b7b54dcf724efb854c19bd13535
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-03-05 00:44:45 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,77 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template Start
-    </div>
-  );
-};
+import { Component } from 'react';
+import initialContacts from '../initialContacts.json';
+
+import { Section } from 'components/Section/Section';
+import { PhonebookForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { PhoneTitle } from './PhoneTitle/PhoneTitle';
+
+export class App extends Component {
+  state = {
+    contacts: initialContacts,
+    filter: '',
+  };
+
+  addContact = contact => {
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, contact],
+    }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  handleFilterChange = event => {
+    const { value } = event.currentTarget;
+
+    this.setState({ filter: value });
+  };
+
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedValue = filter.toLowerCase().trim();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedValue)
+    );
+  };
+
+  clearFilterField = () => {
+    this.setState({ filter: '' });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+
+    return (
+      <>
+        <PhoneTitle name="PhoneBook 2"></PhoneTitle>
+        <Section title="Add new contacts">
+          <PhonebookForm
+            contacts={contacts}
+            addContact={this.addContact}
+          ></PhonebookForm>
+        </Section>
+
+        <Section title="Filter contacts">
+          <Filter
+            onChange={this.handleFilterChange}
+            value={filter}
+            onClick={this.clearFilterField}
+          ></Filter>
+        </Section>
+
+        <Section title="Contact List">
+          <ContactList
+            contactList={this.getFilteredContacts()}
+            onDelete={this.deleteContact}
+          ></ContactList>
+        </Section>
+      </>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { GlobalStyle } from 'components/GlobalStyle';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
-import './index.css';
+import { theme } from './components/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
